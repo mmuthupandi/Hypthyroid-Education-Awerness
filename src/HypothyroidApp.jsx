f3e82c01f66bddbcfe2788dd414c8d1438f93fd8
Muthupandi M <mmuthupandi05@outlook.com>
2025-06-14 13:30:22 +0530

    Update HypothyroidApp.jsx

--- src/HypothyroidApp.jsx
+++ src/HypothyroidApp.jsx
@@ -125,7 +125,7 @@ const HypothyroidApp = () => {
         }}
       >
         <img 
-          src="https://images.unsplash.com/photo-1559757148-5c350d0d3c56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80" 
+          src="\assets\info-en.jpg" 
           alt="Hypothyroid Education" 
           style={{
             width: "100%",
