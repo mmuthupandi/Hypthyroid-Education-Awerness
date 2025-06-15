c867b38ca7ca664f3fd99517f476dcea466ae821
Muthupandi M <mmuthupandi05@outlook.com>
2025-06-15 16:14:50 +0530

    Update HypothyroidAppTamil.jsx

--- src/HypothyroidAppTamil.jsx
+++ src/HypothyroidAppTamil.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import myImage from './assets/info-ta.jpg';
+import myImage from './assets/info-en.translated.jpg';
 import thyroidImg from './assets/thyroid.jpg';
 import junkFoodImg from './assets/junkfood.jpg'; // ✅ new image import
 
